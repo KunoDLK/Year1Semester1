75b514c957aef8f5a23d7358e6dfd633d12b9f01
KunoDLK <kunodlk@gmail.com>
2022-10-13 10:49:35 +0100

    completed creation of subscription

--- SubscriptionManager/src/subscriptionmanager/Subscription.java
+++ SubscriptionManager/src/subscriptionmanager/Subscription.java
@@ -11,6 +11,8 @@ package subscriptionmanager;
  */
 public class Subscription {
 
+    // #region Enums
+
     public enum SubPackages {
         Bronze,
         Silver,
@@ -22,6 +24,10 @@ public class Subscription {
         Monthly
     }
 
+    // #endregion Enums
+
+    // #region Final Static Arrays
+
     public final static double[][] SubscriptionRates = { { 6, 5, 4, 3 }, { 8, 7, 6, 5 }, { 9.99, 8.99, 7.99, 6.99 } };
 
     public final static char[] PackageLetters = { 'B', 'S', 'G' };
@@ -30,24 +36,37 @@ public class Subscription {
 
     public final static int[] PackageDurations = { 1, 3, 6, 12 };
 
-    public String StartDate;
+    // #endregion Final Static Arrays
+
+    // #region Class Properties
+
+    private String DiscountCode;
+
+    private double Cost;
 
     public int Duration;
 
-    private String DiscountCode;
+    public String StartDate;
+
+    public String Name;
 
     public SubPackages SubPackage;
 
     public PaymentTerms PaymentTerm;
 
-    public String Name;
+    // #endregion Class Properties
+
+    // #region Constructor
 
     public Subscription() {
         DiscountCode = "-";
     }
 
-    public String GetDiscoutCode()
-    {
+    // #endregion Constructor
+
+    // #region Public Methods
+
+    public String GetDiscountCode() {
         return DiscountCode;
     }
 
@@ -62,9 +81,14 @@ public class Subscription {
         return null;
     }
 
+    /**
+     * Validates Discount code,
+     * will set discount code if valid
+     * 
+     * @param discountCode as a string
+     * @return true/false if code is valid
+     */
     public boolean ValidateAndSetDiscountCode(String discountCode) {
-
-        DiscountCode = "-";
 
         // code length validation
         if (discountCode.length() != 6)
@@ -100,36 +124,93 @@ public class Subscription {
         int currentMonth = DateHelper.GetCurrentMonth();
         char monthChar = discountCode.charAt(4);
         char validChar;
-        if (currentMonth > 6)
-        {
+
+        if (currentMonth > 6) {
             validChar = 'L';
-        }
-        else
-        {
+        } else {
             validChar = 'E';
         }
 
+        // Checks the month letter matches the current month letter
         if (monthChar != validChar)
             return false;
 
-            try {
+        try {
 
-                String discountPercentageChar = discountCode.substring(5);
-                int discountPercentage = Integer.parseInt(discountPercentageChar);
-    
-                if (discountPercentage < 1 && discountPercentage > 9)
-                    return true;
-    
-            } catch (NumberFormatException e) {
-    
-                return false;
-            }
+            // parses discount % and validates it's in range
+            String discountPercentageChar = discountCode.substring(5);
+            int discountPercentage = Integer.parseInt(discountPercentageChar);
 
+            if (discountPercentage < 1 && discountPercentage > 9)
+                return true;
 
+        } catch (NumberFormatException e) {
+
+            return false;
+        }
+
+        // if code has made it this far code is valid
         DiscountCode = discountCode;
         return true;
     }
 
+    /**
+     * Calculates the cost of the subscription
+     * and sets the start date to today
+     */
+    public boolean StartSubscription() {
+
+        int subPackageIndex;
+
+        if (this.SubPackage != null) {
+            subPackageIndex = this.SubPackage.ordinal();
+
+        } else {
+
+            System.out.println("ERROR, you can not start subscription. SubPackage not Set");
+            return false;
+        }
+
+        int durationIndex = -1;
+        for (int i = 0; i <= 3; i++) {
+            if (PackageDurations[i] == this.Duration) {
+                durationIndex = i;
+            }
+        }
+
+        if (durationIndex == -1) {
+            System.out.println("ERROR, can not start subscription. package duration not set correctly");
+            return false;
+        }
+
+        this.Cost = SubscriptionRates[subPackageIndex][durationIndex];
+
+        if (PaymentTerm == PaymentTerms.OneOff) {
+            this.Cost *= this.Duration;
+            this.Cost *= 0.95;
+        }
+
+        if (!DiscountCode.equals("-")) {
+            char discountChar = DiscountCode.charAt(5);
+            Double discount = Double.parseDouble("0.0" + String.valueOf(discountChar));
+            this.Cost *= (1 - discount);
+        }
+
+        StartDate = DateHelper.GetCurrentDate();
+
+        return true;
+    }
+
+    // #endregion Public Methods
+
+    // #region Private Methods
+
+    /**
+     * Checks if a character is a english upper case letter A-Z
+     * 
+     * @param letter letter in question
+     * @return true/false if is a letter
+     */
     private boolean isLetterAToZ(char letter) {
 
         if (letter >= 'A' && letter <= 'Z') {
@@ -138,4 +219,10 @@ public class Subscription {
             return false;
         }
     }
+
+    public double GetCost() {
+        return this.Cost;
+    }
+
+    // #endregion Private Methods
 }
