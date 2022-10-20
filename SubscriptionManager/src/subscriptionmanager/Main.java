9b77a27bac8d12e6b0b20f658c80da790295c81d
KunoDLK <kunodlk@gmail.com>
2022-10-20 16:59:41 +0100

    started writing write subscription

--- SubscriptionManager/src/subscriptionmanager/Main.java
+++ SubscriptionManager/src/subscriptionmanager/Main.java
@@ -50,6 +50,7 @@ public class Main {
                 case 1:
                     Subscription newSubscription = CreateNewSubscription();
                     consoleMethods.DisplaySubscription(newSubscription);
+                    FileIO.AddSubscriptionToFile(newSubscription);
                     break;
 
                 case 2:
