2d8dbee75b704e84f184ce316ee27e39e70b8215
KunoDLK <kunodlk@gmail.com>
2022-10-20 17:00:24 +0100

    started writing write sub

--- SubscriptionManager/src/subscriptionmanager/FileIO.java
+++ SubscriptionManager/src/subscriptionmanager/FileIO.java
@@ -1,10 +1,11 @@
 package subscriptionmanager;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class FileIO {
 
@@ -12,14 +13,14 @@ public class FileIO {
 
                 ArrayList<Subscription> SubscriptionList = new ArrayList<Subscription>();
                 try {
-                        File myObj = new File("SubscriptionManager/current.txt");
-                        
-                        if (!myObj.isFile()) {
-                                myObj.createNewFile();
+                        File file = new File("SubscriptionManager/current.txt");
+
+                        if (!file.isFile()) {
+                                file.createNewFile();
                                 return SubscriptionList;
                         }
 
-                        Scanner myReader = new Scanner(myObj);
+                        Scanner myReader = new Scanner(file);
                         while (myReader.hasNextLine()) {
                                 Subscription insertSubscription = new Subscription(myReader.nextLine());
                                 SubscriptionList.add(insertSubscription);
@@ -36,5 +37,27 @@ public class FileIO {
                 }
 
                 return SubscriptionList;
+        }
+
+        public static void AddSubscriptionToFile(Subscription subscription) {
+
+                String fileString = "";
+
+                try {
+
+                        File file = new File("SubscriptionManager/current.txt");
+
+                        if (!file.isFile()) {
+                                file.createNewFile();
+                        }
+
+                        FileWriter fileWriter = new FileWriter("SubscriptionManager/current.txt", true);
+                        fileWriter.append(fileString);
+                        fileWriter.close();
+
+                } catch (IOException e) {
+                        System.out.println("Failed to create file");
+                        e.printStackTrace();
+                }
         }
 }
