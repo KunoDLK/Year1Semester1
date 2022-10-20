737fb074b592d446ca4b16153651833361ac6dce
KunoDLK <kunodlk@gmail.com>
2022-10-20 16:28:39 +0100

    completed display summaries

--- SubscriptionManager/src/subscriptionmanager/Main.java
+++ SubscriptionManager/src/subscriptionmanager/Main.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import subscriptionmanager.Subscription.PaymentTerms;
+import subscriptionmanager.Subscription.SubPackages;
 
 /**
  *
@@ -76,22 +77,50 @@ public class Main {
         ArrayList<Subscription> subscriptionList = FileIO.GetAllSubscriptions();
         System.out.println("...DONE");
 
-        double totalCost = 0;
-        HashMap<String, HashMap<Subscription.SubPackages, ArrayList<Subscription>>> orginisedSubscriptions = new HashMap<String, HashMap<Subscription.SubPackages, ArrayList<Subscription>>>();
+        HashMap<String, HashMap<SubPackages, ArrayList<Subscription>>> organizedSubscriptions = Subscription
+                .OrginiseSubscriptions(subscriptionList);
 
-        orginisedSubscriptions.put("Jan", null)
-
+        float totalCost = 0;
         for (Subscription subscription : subscriptionList) {
             totalCost += subscription.GetCost();
+        }
+
+        String months = "Month:\t";
+        String totalSubs = "Total:\t";
+        String bronzeSubs = "Bronze:\t";
+        String silverSubs = "Silver:\t";
+        String goldSubs = "Gold\t";
+
+        for (String month : DateHelper.Months) {
+
+            HashMap<SubPackages, ArrayList<Subscription>> monthSubs = organizedSubscriptions.get(month);
+            int monthBronzeSubs = monthSubs.get(Subscription.SubPackages.Bronze).size();
+            int monthSilverSubs = monthSubs.get(Subscription.SubPackages.Silver).size();
+            int monthGoldSubs = monthSubs.get(Subscription.SubPackages.Gold).size();
+
+            months += month + "\t";
+            bronzeSubs += monthBronzeSubs + "\t";
+            silverSubs += monthSilverSubs + "\t";
+            goldSubs += monthGoldSubs + "\t";
+            totalSubs += (monthBronzeSubs + monthSilverSubs + monthGoldSubs) + "\t";
 
         }
 
         System.out.print("Total number of subscriptions: ");
         System.out.println(subscriptionList.size());
 
+        System.out.print("Average monthly subscription: ");
+        System.out.println((float) subscriptionList.size() / 12);
+
         System.out.print("Average monthly subscription fee: ");
         System.out.println(totalCost / subscriptionList.size());
 
+        System.out.println(months);
+        System.out.println(totalSubs);
+        System.out.println(bronzeSubs);
+        System.out.println(silverSubs);
+        System.out.println(goldSubs);
+        System.out.println();
     }
 
     private static void FindUserAndDisplay() {
@@ -99,6 +128,60 @@ public class Main {
     }
 
     private static void SelectMonthsSubscriptions() {
+
+        float totalCost = 0;
+
+        System.out.print("Select month (1-12): ");
+        int month = consoleMethods.GetValidatedInteger(1, 12);
+
+        System.out.print("Reading file");
+        ArrayList<Subscription> subscriptionList = FileIO.GetAllSubscriptions();
+        System.out.println("...DONE");
+
+        HashMap<String, HashMap<SubPackages, ArrayList<Subscription>>> organizedSubscriptions = Subscription
+                .OrginiseSubscriptions(subscriptionList);
+
+        HashMap<SubPackages, ArrayList<Subscription>> monthSubs = organizedSubscriptions
+                .get(DateHelper.Months[month - 1]);
+
+        ArrayList<Subscription> monthBronzeSubs = monthSubs.get(Subscription.SubPackages.Bronze);
+        ArrayList<Subscription> monthSilverSubs = monthSubs.get(Subscription.SubPackages.Silver);
+        ArrayList<Subscription> monthGoldSubs = monthSubs.get(Subscription.SubPackages.Gold);
+        
+        for (Subscription subscription : monthBronzeSubs) {
+            totalCost += subscription.GetCost();
+        }
+        for (Subscription subscription : monthSilverSubs) {
+            totalCost += subscription.GetCost();
+        }
+        for (Subscription subscription : monthGoldSubs) {
+            totalCost += subscription.GetCost();
+        }
+
+        int totalSubs = monthBronzeSubs.size() + monthSilverSubs.size() + monthGoldSubs.size();
+
+        System.out.print("Total number of subscriptions for ");
+        System.out.print(DateHelper.Months[month - 1]);
+        System.out.print(" : ");
+        System.out.println(totalSubs);
+
+        System.out.print("Average subscription fee: £ ");
+        System.out.println(totalCost / totalSubs);
+        System.out.println(2);
+
+        System.out.println("Percentage of subscriptions:");
+        System.out.print("Bronze: ");
+        System.out.print(monthBronzeSubs.size() / (double) totalSubs * 100);
+        System.out.println(" %");
+
+        System.out.print("Silver: ");
+        System.out.print(monthSilverSubs.size() / (double) totalSubs * 100);
+        System.out.println(" %");
+
+        System.out.print("Gold: ");
+        System.out.print(monthGoldSubs.size() / (double) totalSubs * 100);
+        System.out.println(" %");
+        System.out.println();
 
     }
 
